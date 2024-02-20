392ff7d65ac2ccb8a79f29604a2a7116809a5b6c
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2024-02-20 10:44:10 +0530

    created mongodb connection object and  database models

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,44 @@
+import { Document, Schema, model, models } from "mongoose";
+
+
+export interface IImage extends Document {
+    title: string;
+    transformationType: string;
+    publicId: string;
+    secureUrl: string;
+    width?: number; // Optional since width is not marked as required
+    height?: number; // Optional since height is not marked as required
+    config?: any; // Object type in Schema, but could be more specific if the shape of config is known
+    transformationUrl?: URL; // Optional since transformationUrl is not marked as required
+    aspectRatio?: string; // Optional since aspectRatio is not marked as required
+    color?: string; // Optional since color is not marked as required
+    prompt?: string; // Optional since prompt is not marked as required
+    author: {
+        _id: string;
+        firstName: string;
+        lastName: string;
+    } // Using ObjectId type from MongoDB
+    createdAt?: Date; // Default value is provided, so it's automatically set but can be optional in interface
+    updatedAt?: Date; // Default value is provided, so it's automatically set but can be optional in interface
+}
+
+const ImageSchema = new Schema({
+    title: { type: String, required: true },
+    transformationType: { type: String, required: true },
+    publicId: { type: String, required: true },
+    secureUrl: { type: String, required: true },
+    width: { type: Number },
+    height: { type: Number },
+    config: { type: Object },
+    transformationUrl: { type: URL },
+    aspectRatio: { type: String },
+    color: { type: String },
+    prompt: { type: String },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+const Image = models?.Image || model('Image', ImageSchema);
+
+export default Image;
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,29 @@
+// clearId, email, username, photo, firstName, lastName, planId, creditBalance
+import { Document, Schema, model, models } from "mongoose";
+
+
+export interface IUser extends Document {
+    clearId: string;
+    email: string;
+    username: string;
+    photo?: string; // Optional since photo is not explicitly marked as required
+    firstName: string;
+    lastName: string;
+    planId: string;
+    creditBalance: number;
+}
+
+const UserSchema = new Schema({
+    clearId: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    photo: { type: String, required: true }, // Optional
+    firstName: { type: String },
+    lastName: { type: String },
+    planId: { type: String, default: 1 },
+    creditBalance: { type: Number, default: 10 },
+}); // Enable createdAt and updatedAt fields automatically
+
+const User = models?.User || model('User', UserSchema);
+
+export default User;
