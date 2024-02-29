598a3cc67bd1cc81f5335a36897a5588ea928789
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2024-02-29 20:55:28 +0530

    added select aspect ration option for images

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -21,8 +21,10 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { defaultValues } from "@/constants"
+import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants"
 import { CustomField } from "./CustomField"
+import { useState } from "react"
+import { AspectRatioKey } from "@/lib/utils"
 
 
 export const formSchema = z.object({
@@ -33,8 +35,12 @@ export const formSchema = z.object({
   publicId: z.string(),
 })
 
-const TransformationForm = ({ action, data = null }:
-  TransformationFormProps ) => {
+const TransformationForm = ({ action, data = null, userId, type, creditBalance }:
+  TransformationFormProps) => {
+  const transformationType = transformationTypes[type];
+  const [Image, setImage] = useState(data);
+  const [newTransformation, setNewTransformation] = useState<Transformations | null>(null);
+
   const initialValues = data && action === 'Update' ? {
     title: data?.title,
     aspectRation: data?.aspectRation,
@@ -48,24 +54,59 @@ const TransformationForm = ({ action, data = null }:
     resolver: zodResolver(formSchema),
     defaultValues: initialValues,
   })
- 
+
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
+
+  const onSelectFieldHandler = (
+    value: string,
+    onChangeField: (value: string) => void) => {
+
+  }
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} 
-      className="space-y-8">
-        <CustomField 
+      <form onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8">
+        <CustomField
           control={form.control}
           name="title"
           formLabel="Image Title"
           className="w-full"
           render={({ field }) => <Input {...field} className="input-field" />}
         />
+
+        {type === 'fill' && (
+          <CustomField
+            control={form.control}
+            name="aspectRatio"
+            formLabel="Aspect Ratio"
+            className="w-full"
+            render={({ field }) => (
+              <Select
+                onValueChange={(value) =>
+                  onSelectFieldHandler(value, field.onChange)}
+              >
+                <SelectTrigger className="select-field">
+                  <SelectValue placeholder="Select size" />
+                </SelectTrigger>
+                <SelectContent>
+                  {Object.keys(aspectRatioOptions).map((key) => (
+                    <SelectItem key={key} value={key} className="select-item">
+                      {aspectRatioOptions[key as AspectRatioKey].label}
+
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+
+            )}
+          />
+        )}
       </form>
     </Form>
   )
